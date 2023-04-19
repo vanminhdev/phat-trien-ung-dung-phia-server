92d2adbc1de06681066b86a336e82b64cf10938e
vanminh <vanminh.dev@gmail.com>
2023-04-19 14:36:14 +0700

    demo

---
+++ 65CS/65CS3/DemoWebAPI/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dto;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("get-by-id/{id}")]
+        public string Get(int id, string name, int age)
+        {
+            return "value";
+        }
+
+        [HttpGet("get-by-id2/{id}")]
+        public string Get2(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost("create")]
+        public CreateValueDto Post(CreateValueDto input)
+        {
+            return input;
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 65CS/65CS3/DemoWebAPI/WebApplication1/Dto/CreateValueDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Dto
+{
+    public class CreateValueDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
