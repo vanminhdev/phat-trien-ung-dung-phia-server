ac9d0bf17a3a06695dc09deef6374358259a00c5
vanminh <vanminh.dev@gmail.com>
2023-04-21 13:39:10 +0700

    demo validation

---
+++ 65CS/65CS1/WebAPIDemo/WebApplication1/Validations/StringRangeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Validations
+{
+    public class StringRangeAttribute : ValidationAttribute
+    {
+        public string[] AllowableValues { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || AllowableValues?.Contains(value?.ToString()) == true)
+            {
+                return ValidationResult.Success;
+            }
+            ErrorMessage ??= "Chỉ được chọn những giá trị sau {0}";
+            var msg = string.Format(ErrorMessage, string.Join(", ", AllowableValues));
+            return new ValidationResult(msg);
+        }
+    }
+}
